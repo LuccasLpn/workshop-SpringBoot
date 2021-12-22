ef7f6604f7bf8fc8cbefe59c122bb93a4ae01a65
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-22 16:14:47 -0300

    Order, Instant, ISO 8601 2

---
+++ src/main/java/com/example/course/services/OrderSerrvice.java
@@ -0,0 +1,29 @@
+package com.example.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.Order;
+import com.example.course.repositories.OrderRepository;
+
+@Service
+public class OrderSerrvice {
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	public List<Order> findAll() {
+
+		return orderRepository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+
+	}
+
+}
