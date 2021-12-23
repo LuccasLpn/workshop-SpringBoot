7dbb97ad7f1e257fb48b734b25019fe491651a17
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-23 13:42:36 -0300

    User delete

--- src/main/java/com/example/course/services/UserSerrvice.java
+++ src/main/java/com/example/course/services/UserSerrvice.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
@@ -27,6 +26,10 @@ public class UserSerrvice {
 	
 	public User insert(User obj) {
 		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		userRepository.getById(id);
 	}
 
 }
