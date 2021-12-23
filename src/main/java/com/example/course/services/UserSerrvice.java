1ad5c27dd5aab66f538b2771e1225721349423cb
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-23 14:53:26 -0300

    Exception handling - delete

--- src/main/java/com/example/course/services/UserSerrvice.java
+++ src/main/java/com/example/course/services/UserSerrvice.java
@@ -1,14 +1,16 @@
 package com.example.course.services;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
+import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -20,21 +22,36 @@ public class UserSerrvice {
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-		Optional <User> obj = userRepository.findById(id);
-		return obj.orElseThrow( () -> new ResourceNotFoundException(id));
+
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		userRepository.getById(id);
+		try {
+
+			userRepository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+
+			throw new ResourceNotFoundException(id);
+
+		} catch (DataIntegrityViolationException er) {
+
+			throw new DatabaseException(er.getMessage());
+
+		}
+
 	}
-	
-	public User update(Long id,  User obj) {
+
+	public User update(Long id, User obj) {
 		@SuppressWarnings("deprecation")
 		User entity = userRepository.getOne(id);
 		updatedata(entity, obj);
@@ -45,7 +62,7 @@ public class UserSerrvice {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 
 }
